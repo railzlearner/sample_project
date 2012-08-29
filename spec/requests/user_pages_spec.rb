76fa6276b206bcb2da2f507921c8a7c470e73dfb
Amit Nangare <anangare@cisco.com>
2012-08-28 23:23:31 -0700

    Added signup page with proper error handling and tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,8 +4,42 @@ describe "UserPages" do
 
   subject { page }
 
-  describe "signup page" do
+  describe "signup" do
     before { visit signup_path }
+
+    let(:submit) { "Create my account" }
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+    end
+
+    #describe "with invalid information" do
+    #  before do
+    #    fill_in "Name",           with: "Example User"
+    #    fill_in "Email",          with: "user+example.com"
+    #    fill_in "Password",       with: "foo"
+    #    fill_in "Confirmation",   with: "foo"
+    #  end
+    #  it "should have the error messages" do
+    #    click_button
+    #    it { should have_selector(text: 'Email is invalid') }
+    #    it { should have_selector(text: "Password is too short (minimum is 6 characters)") }
+    #  end
+    #end
+
+    describe "with vaild information" do
+      before do
+        fill_in "Name",           with: "Example User"
+        fill_in "Email",          with: "user@example.com"
+        fill_in "Password",       with: "foobar"
+        fill_in "Confirmation",   with: "foobar"
+      end
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+    end
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
