8713851ef06e4cd549a055ac9acdf397370f2023
Amit Nangare <anangare@cisco.com>
2012-08-29 14:26:18 -0700

    Add sessions controller, sign in page, action and handle sign in failure

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ SampleProject::Application.routes.draw do
 
   match '/signup',  to: 'users#new'
 
+  match '/signin',  to: 'sessions#new'
+
+  match '/signout',  to: 'sessions#destroy', via: :delete
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
