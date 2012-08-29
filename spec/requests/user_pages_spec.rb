e2c3a1b0f2aa889be860e7dd6850fe7aeef1940a
Amit Nangare <anangare@cisco.com>
2012-08-28 23:49:56 -0700

    Add tests for post save and error message Cleaned up some code

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,21 +13,15 @@ describe "UserPages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
-    end
 
-    #describe "with invalid information" do
-    #  before do
-    #    fill_in "Name",           with: "Example User"
-    #    fill_in "Email",          with: "user+example.com"
-    #    fill_in "Password",       with: "foo"
-    #    fill_in "Confirmation",   with: "foo"
-    #  end
-    #  it "should have the error messages" do
-    #    click_button
-    #    it { should have_selector(text: 'Email is invalid') }
-    #    it { should have_selector(text: "Password is too short (minimum is 6 characters)") }
-    #  end
-    #end
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
+
+    end
 
     describe "with vaild information" do
       before do
@@ -39,14 +33,15 @@ describe "UserPages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
-    end
 
-    #describe "with vaild information" do
-    #  before let(:user) { FactoryGirl.create(:user)}
-    #  it "should flash the success message" do
-    #    it { should have_selector('div',    text: 'Welcome to the Sample App!') }
-    #  end
-    #end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
