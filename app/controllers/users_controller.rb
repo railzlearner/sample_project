15d4a2c660a3978b484109225c145baf5771f968
Amit Nangare <anangare@cisco.com>
2012-08-29 17:35:05 -0700

    The user should be able to edit or update only his profile and routed to root_url if he tries to edit someone elses profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -23,11 +24,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
       # Handle a successful update.
       flash[:success] = "Profile updated"
@@ -42,6 +41,11 @@ class UsersController < ApplicationController
 
   def signed_in_user
     redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
