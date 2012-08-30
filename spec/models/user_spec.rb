453c17d281288f950428756077707773a04e9856
Amit Nangare <anangare@cisco.com>
2012-08-29 23:51:00 -0700

    Add migration for adding admin column to users Added delete functionality for admin users only verified that regular users cant delete anyone

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,9 +26,20 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
 
   describe "when name is missing" do
     before { @user.name = " " }
@@ -126,4 +137,6 @@ describe User do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
   end
+
+
 end
