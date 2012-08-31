cabc763880cce9a913b3b800ce6bf9be3d81d107
Amit Nangare <anangare@cisco.com>
2012-08-31 16:49:59 -0700

    complete micropost display Added destroy feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,6 +26,10 @@ class User < ActiveRecord::Base
 
   validates :password_confirmation, presence: true
 
+  def feed
+    microposts
+  end
+
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
